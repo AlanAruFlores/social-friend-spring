0b4cc67cbc61b628e6e565334120cf3eaecd728f
AlanAruFlores <alanaruquipa242@gmail.com>
2024-08-01 23:33:47 -0300

    primer commit

---
+++ src/main/java/com/ar/social_friend/social_friend/SocialFriendApplication.java
@@ -0,0 +1,13 @@
+package com.ar.social_friend.social_friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ar/social_friend/social_friend/SocialFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.social_friend.social_friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
