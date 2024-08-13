8b33ff0820206723bc882e7df6487b46bf29c8cf
AlanAruFlores <alanaruquipa242@gmail.com>
2024-08-13 14:45:02 -0300

    controller search impl

--- src/main/java/com/ar/social_friend/social_friend/controllers/HomeControlller.java
+++ src/main/java/com/ar/social_friend/social_friend/controllers/HomeControlller.java
@@ -1,15 +1,42 @@
 package com.ar.social_friend.social_friend.controllers;
 
+import com.ar.social_friend.social_friend.domain.User;
+import com.ar.social_friend.social_friend.exceptions.ResultsNotFoundException;
+import com.ar.social_friend.social_friend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/home")
 public class HomeControlller {
+
+    private UserService userService;
+
+    @Autowired
+    public HomeControlller(UserService userService){
+        this.userService  = userService;
+    }
 
     @RequestMapping("/")
     public String home() {
         return "home";
     }
 
+    @RequestMapping(value = "/search-users", method = RequestMethod.POST)
+    public String searchUsers(String toSearch, Model model){
+
+        try{
+            List<User> results = userService.searchUsersByUsername(toSearch);
+            model.addAttribute("results", results);
+        }catch (ResultsNotFoundException ex){
+            model.addAttribute("error", "No hay resultados");
+        }
+
+        return "users_search";
+    }
 }
--- src/test/java/com/ar/social_friend/social_friend/controllers/HomeControllerTest.java
+++ src/test/java/com/ar/social_friend/social_friend/controllers/HomeControllerTest.java
@@ -1,11 +1,20 @@
 package com.ar.social_friend.social_friend.controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.ar.social_friend.social_friend.DataProvider;
+import com.ar.social_friend.social_friend.exceptions.ResultsNotFoundException;
+import com.ar.social_friend.social_friend.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.Model;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -15,7 +24,15 @@ public class HomeControllerTest {
     @InjectMocks
     private HomeControlller homeControlller;
 
+    @Mock
+    private UserService userService;
 
+    private Model model;
+
+    @BeforeEach
+    void init(){
+        this.model = mock(Model.class);
+    }
 
     @Test
     public void testGoToMainHomePage(){
@@ -25,5 +42,24 @@ public class HomeControllerTest {
 
     private String whenGoToHomePage() {
         return this.homeControlller.home();
+    }
+
+    @Test
+    public void testSearchUsers() throws ResultsNotFoundException {
+        String toSearch="name";
+
+        when(this.userService.searchUsersByUsername(toSearch)).thenReturn(DataProvider.getUsers());
+
+        String view = this.homeControlller.searchUsers(toSearch,model);
+        assertEquals("users_search", view);
+    }
+    @Test
+    public void testNotSearchUsers() throws ResultsNotFoundException {
+        String toSearch="name";
+
+        when(this.userService.searchUsersByUsername(toSearch)).thenThrow(ResultsNotFoundException.class);
+
+        String view = this.homeControlller.searchUsers(toSearch,model);
+        assertEquals("users_search", view);
     }
 }
